f9c3d503f0ffe3ab5e09879cef43b6a6596ce5f9
Ralf Bach <rkb@customerwise.dk>
2016-01-16 07:02:52 +0100

    Added more test

--- test/spec/spec-utils.js
+++ test/spec/spec-utils.js
@@ -19,15 +19,18 @@ describe('k.utils', function () {
     describe('k.utils.contains', function () {
         var s = "this is a string";
         var a = ["this", "is", "an", "array"];
+        var a1 = [1, 2, 3, 4];
 
         it('should return true if value is found in string or array', function() {
             expect(k.utils.contains(s, "is")).toBe(true);
             expect(k.utils.contains(a, "is")).toBe(true);
+            expect(k.utils.contains(a1, 2)).toBe(true);
         });
 
         it('and false if it is not found', function() {
             expect(k.utils.contains(s, "cirkus")).toBe(false);
             expect(k.utils.contains(a, "cirkus")).toBe(false);
+            expect(k.utils.contains(a1, 5)).toBe(false);
         });
     });
 
--- test/spec/spec.extension.js
+++ test/spec/spec.extension.js
@@ -1,20 +1,24 @@
 describe('k.extensions', function () {
 
-    it('Should uppercase first letter in string', function () {
-        var s = "hepper ding";
+    describe("String.upperCaseFirst", function (){
+        it('Should uppercase first letter in string', function () {
+            var s = "hepper ding";
 
-        expect(s.upperCaseFirst()).toBe("Hepper ding");
+            expect(s.upperCaseFirst()).toBe("Hepper ding");
+        });
     });
 
-    it("Should return thousand separated number", function() {
-        var n = 1234;
-        expect(n.toStringWithThousandSeparator()).toBe("1.234");
+    describe("String.toStringWithThousandSeparator", function() {
+        it("Should return thousand separated number", function() {
+            var n = 1234;
+            expect(n.toStringWithThousandSeparator()).toBe("1.234");
 
-        n = 1234567;
-        expect(n.toStringWithThousandSeparator()).toBe("1.234.567");
+            n = 1234567;
+            expect(n.toStringWithThousandSeparator()).toBe("1.234.567");
 
-        n = 1234567.22;
-        expect(n.toStringWithThousandSeparator()).toBe("1.234.567,22");
+            n = 1234567.22;
+            expect(n.toStringWithThousandSeparator()).toBe("1.234.567,22");
+        });
     });
 
     describe("Array.clone", function() {
@@ -26,6 +30,18 @@ describe('k.extensions', function () {
 
         it("but with clone, it should not be the same array", function() {
             expect(a.clone()).not.toBe(a);
+        });
+    });
+
+    describe("Array.contains", function() {
+        var a = ["hep", "dep", 2, 4];
+
+        it("should return true if found", function() {
+            expect(a.contains(2)).toBe(true);
+        });
+
+        it("and false if not found", function() {
+            expect(a.contains("ding")).toBe(false);
         });
     });
 });
